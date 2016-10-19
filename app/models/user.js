496f622e41b745aa61df9ff3bc1f43ebe05d3a22
francislagueu <lagson6@yahoo.fr>
2016-10-18 20:26:39 -0700

    initial commit

---
+++ app/models/user.js
@@ -0,0 +1,47 @@
+/**
+ * Created by Francis on 10/17/2016.
+ */
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+var userSchema = mongoose.Schema({
+    local :{
+        email:String,
+        password:String
+    },
+    facebook:{
+        id:String,
+        token:String,
+        email:String,
+        name:String
+    },
+    twitter:{
+        id:String,
+        token:String,
+        displayName:String,
+        username:String
+    },
+    google:{
+        id:String,
+        token:String,
+        email:String,
+        name:String
+    },
+    spotify:{
+        id:String,
+        token:String,
+        email:String,
+        display_name:String,
+    }
+
+});
+
+userSchema.methods.generateHash = function(password){
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(9), null);
+};
+
+userSchema.methods.validPassword = function(password){
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
---
+++ config/auth.js
@@ -0,0 +1,22 @@
+module.exports = {
+    'facebookAuth':{
+        'clientID': '1636040456692999',
+        'clientSecret': '44e4cd1cb0a85bce65db8fabdd7d9db7',
+        'callbackURL': 'http://localhost:3000/auth/facebook/callback'
+    },
+    'googleAuth':{
+        'clientID': ' 15027467851-6f9637iann3pstdstg78gi87gut5gdkc.apps.googleusercontent.com',
+        'clientSecret': ' vF-nIkgRYGDebKXguWaFMOqT',
+        'callbackURL': 'http://localhost:3000/auth/google/callback'
+    },
+    'twitterAuth':{
+        'consumerKey': 'WLWGVl3MDRBW9g1Uc9fug8Hnc',
+        'consumerSecret': ' 5QkATkpeuMds4jYBXX7iA3ZyFC6HtUl4xJlger0heimHNKpJC0',
+        'callbackURL': 'http://localhost:3000/auth/twitter/callback'
+    },
+    'spotifyAuth':{
+        'clientID': '562bda68b1f04674904f53614456a49b',
+        'clientSecret': '21e398a783a64d1da033b66bb5c2a817',
+        'callbackURL': 'http://localhost:3000/auth/spotify/callback'
+    }
+}
