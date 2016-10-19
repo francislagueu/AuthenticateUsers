276dde1d5ad96fc2ecab41f83048bd2e278e991d
francislagueu <lagson6@yahoo.fr>
2016-10-18 21:17:27 -0700

    finished the facebook authentication

--- app/routes.js
+++ app/routes.js
@@ -19,6 +19,12 @@ module.exports = function (app, passport) {
         res.render('profile.ejs',{ user: req.user });
     });
 
+    app.get('/auth/facebook', passport.authenticate('facebook', {scope:'email'}));
+    app.get('/auth/facebook/callback', passport.authenticate('facebook',{
+        successRedirect:'/profile',
+        failureRedirect:'/'
+    }));
+
     app.get('/logout', function(req, res){
         req.logout();
         res.redirect('/');
